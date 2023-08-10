d1773750fce4dfa0552f58bfe442e94978b40257
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-08-10 20:39:51 +0600

    30. Adding & Configuring the Submit button

--- fitness-tracker/src/app/auth/signup/signup.component.ts
+++ fitness-tracker/src/app/auth/signup/signup.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  OnClick() {
+    console.log('Submit button clicked!');
+  }
 
+  onSubmit(form: NgForm) {
+    console.log(`email: ${form.value.email} password: ${form.value.password}`);
+  }
 }
